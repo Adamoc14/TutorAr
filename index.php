84edaa7f8f4fc2c2ef4ec6f10f15e724e6bee14c
adamoc <oceallaighadam96@gmail.com>
2019-01-22 18:43:42 +0000

    Adding index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <style>
+        input{
+            width:100vw;
+        }
+    
+    </style>
+</head>
+<body>
+    <h1>What Features Will Your Dog Have ?</h1>
+    <input type="checkbox"><label for="">Main Colour</label>
+    <input type="checkbox"><label for="">Has Collar</label>
+    <input type="checkbox"><label for="">Name</label>
+    <input type="checkbox"><label for="">Age</label>
+    <input type="checkbox"><label for="">Type</label>
+    <input type="checkbox"><label for="">Weight</label>
+    <input type="checkbox"><label for="">Height</label>
+
+    
+</body>
+</html>
