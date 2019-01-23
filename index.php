576fd160e73e41a59a595f13ce0ecd9668680d8a
adamoc <oceallaighadam96@gmail.com>
2019-01-23 08:26:09 +0000

    Styled and got php to display

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
-    $page = ''; 
+    $type = ''; 
     $img = '';
-    $type = '';
-    $labels = [];
+    $typeLabel = '';
+    $labels = array();
 
 
     function strip_bad_chars($input) {
@@ -12,22 +12,43 @@
     }
 
     if(isset($_GET['type'])) {
-        $page = strip_bad_chars($_GET['type']);
-    } 
+        $type = strip_bad_chars($_GET['type']);
+        // echo $type;
+        if ($type == "dog") {
+            $img = 'Images/Dog.jpg';
+            $typeLabel = 'Dog';
+            array_push($labels, "Main Colour", "Has Collar", "Name", "Age", "Type", "Weight", "Height");
+            // echo $img , $typeLabel;
+            // foreach($labels as $label){
+            //     print_r($label);
+            // }
+        } else if ($type == "book") {
+            $img = 'Images/book.jpg';
+            $typeLabel = 'Book';
+            array_push($labels, "Colour Language", "Font", "Number of pages" ,"Title" ,"Price" ,"Weight" ,"Height" ,"Genre");
+            // echo $img , $typeLabel;
+            // foreach($labels as $label){
+            //     print_r($label);
+            // }
+        } else if ($type == "house") {
+            $img = 'Images/house.jpg';
+            $typeLabel = 'House';
+            array_push($labels , "Colour","Door Colour","Number of windows","Type of windows","Number of Storeys","Number of rooms ","Southfacing / Northfacing ");
+            // echo $img , $typeLabel;
+            // foreach($labels as $label){
+            //     print_r($label);
+            // }
 
-    if ($page == "dog") {
-        $img = 'Images/Dog.jpg';
-        $type = 'Dog';
-        $labels = ["Main Colour", "Has Collar", "Name", "Age", "Type", "Weight", "Height"];
-    } else if ($page == "book") {
-        $img = 'Images/book.jpg';
-        $type = 'Book';
-        $labels = ["Colour Language", "Font", "Number of pages" ,"Title" ,"Price" ,"Weight" ,"Height" ,"Genre"];
-    } else if ($page == "house") {
-        $img = 'Images/house.jpg';
-        $type = 'House';
-        $labels = ["Colour","Door Colour","Number of windows","Type of windows","Number of Storeys","Number of rooms ","Southfacing / Northfacing "];
-    } 
+            /*Using these above statements i ouputted all my variables to see what was going on behind the page and I found multiple errors first being undefined index for get variable
+            sorted this by using isset() method then it was telling me my variables weren't noticed so I declared them and then I tried to output all my variables once I 
+            wanted to test whether they were working or not and first two are strings so you can echo as is but for array use foreach() to loop through array and output result*/
+        } 
+    } else {
+        $type = ''; 
+        $img = '';
+        $typeLabel = '';
+        $labels = [];
+    }
     
     
     
@@ -49,23 +70,40 @@
     <title>Document</title>
 
     <style>
-        input{
-            width:100vw;
+
+
+        h1 {
+            display: flex;
+            justify-content: center;
+            font-family: monospace;
+        }
+
+        main{
+            margin: 5vh 1vw;
+        }
+
+        label{
+            margin-left: 1vw;
+        }
+        
+        img {
+            width: 20vw;
+            margin: -49vh auto;
+            display : flex;
         }
     
     </style>
 </head>
 <body>
-    <h1>What Features Will Your Dog Have ?</h1>
     <?php 
 
-        "<h1>What Features Will Your $type Have ?</h1>";
+       echo  "<h1>What Features Will Your $typeLabel Have ?</h1>";
 
         foreach ($labels as $label) {
-            "<input type=\"checkbox\"><label>$label</label>";
+           echo  "<main><input type=\"checkbox\"><label>$label</label></main>";
         }
 
-        "<img src=\"$img\">";
+        echo "<img src=\"$img\">";
     ?>
 
     
