c260e3ad626384bc35143d4eb7c6c6b5a0db2361
adamoc <oceallaighadam96@gmail.com>
2019-03-15 12:27:09 +0000

    Testing

--- inputValues.php
+++ inputValues.php
@@ -40,7 +40,7 @@ $labelsCount = 0;
                 width: 50vw;
                 margin: 0 8vw;
                 min-height: 60vh;
-                height: 60vh;
+                height: 80vh;
             }
 
         }
